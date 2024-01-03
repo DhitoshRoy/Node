170abfbb47ffd85540b4206b356be6c43b7f8b0a
Dhitosh Roy <roydhitosh@gmail.com>
2024-01-03 20:29:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+
+
+const {Console } = require('console');
+const http = require ('http');
+
+const port = 8080;
+
+
+function requestHandler( req, res){
+
+    // Console.log(req.url);
+
+    res.end("Hi! Dhitosh");
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, function(err){
+
+    if (err){
+
+        console.log(err);
+        return;
+    }
+
+
+    console.log("server is running on port:  ", port);
+
+
+});
