f6406088eeee16a0abd150a0089f568ac2704c40
Dhitosh Roy <142252404+DhitoshRoy@users.noreply.github.com>
2024-03-03 19:45:03 +0530

    first serer creation code

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 
+// const {Console } = require('console');
+// const http = require ('http');
+
+// const port = 8080;
 
 
+// function requestHandler( req, res){
 
-const {Console } = require('console');
+//     // Console.log(req.url);
+
+//     res.end("Hi! Dhitosh");
+// }
+
+// const server = http.createServer(requestHandler);
+
+// server.listen(port, function(err){
+
+//     if (err){
+
+//         console.log(err);
+//         return;
+//     }
+
+
+//     console.log("server is running on port:  ", port);
+
+
+// });
+
 const http = require ('http');
+const port = 8000;
 
-const port = 8080;
 
-
-function requestHandler( req, res){
-
-    // Console.log(req.url);
-
-    res.end("Hi! Dhitosh");
-}
-
-const server = http.createServer(requestHandler);
+const server = http.createServer ();
 
 server.listen(port, function(err){
 
@@ -25,8 +42,6 @@ server.listen(port, function(err){
         return;
     }
 
-
-    console.log("server is running on port:  ", port);
-
-
+    console.log("server is running on port : ", port );
+    
 });
